092c46b5a91a820eecdf8fe67bdfe36747063cc6
adrianwit <adrianwit@gmail.com>
2017-09-19 17:27:32 -0700

    added DatasetResource

--- dataset_factory.go
+++ dataset_factory.go
@@ -8,6 +8,8 @@ import (
 
 	"github.com/viant/dsc"
 	"github.com/viant/toolbox"
+	"github.com/viant/toolbox/storage"
+	"path"
 )
 
 type datasetFactoryImpl struct {
@@ -15,6 +17,9 @@ type datasetFactoryImpl struct {
 }
 
 func (f datasetFactoryImpl) ensurePkValues(data map[string]interface{}, descriptor *dsc.TableDescriptor) {
+	if descriptor == nil {
+		return
+	}
 	for _, pkColumn := range descriptor.PkColumns {
 		_, ok := data[pkColumn]
 		if !ok {
@@ -100,19 +105,38 @@ func (f datasetFactoryImpl) buildDatasetFromJSON(descriptor *dsc.TableDescriptor
 }
 
 func (f datasetFactoryImpl) getDescriptor(datastore string, table string) *dsc.TableDescriptor {
-	mnager := f.managerRegistry.Get(datastore)
-	descriptor := mnager.TableDescriptorRegistry().Get(table)
+	manager := f.managerRegistry.Get(datastore)
+	registry := manager.TableDescriptorRegistry()
+	if !registry.Has(table) {
+		dbConfig := manager.Config()
+		dialect := dsc.GetDatastoreDialect(dbConfig.DriverName)
+		key := dialect.GetKeyName(manager, datastore, table)
+		descriptor := &dsc.TableDescriptor{
+			Table:     table,
+			PkColumns: strings.Split(key, ","),
+		}
+		registry.Register(descriptor)
+	}
+	descriptor := registry.Get(table)
 	return descriptor
 }
 
 func (f datasetFactoryImpl) CreateFromURL(datastore string, table string, url string) (*Dataset, error) {
-	descriptor := f.getDescriptor(datastore, table)
 	reader, mimeType, err := toolbox.OpenReaderFromURL(url)
 	if err != nil {
 		return nil, err
 	}
 	defer reader.Close()
+	return f.createDataset(reader, datastore, table, mimeType, url)
+}
 
+func (f datasetFactoryImpl) createDataset(reader io.Reader, datastore, table, mimeType, url string) (*Dataset, error) {
+	descriptor := f.getDescriptor(datastore, table)
+	if descriptor == nil {
+		descriptor = &dsc.TableDescriptor{
+			Table: table,
+		}
+	}
 	if mimeType == "text/csv" {
 		columns, rows := parseColumnarData(reader, ",")
 		return f.buildDataSetFromColumnarData(descriptor, url, columns, rows), nil
@@ -126,6 +150,58 @@ func (f datasetFactoryImpl) CreateFromURL(datastore string, table string, url st
 	}
 
 	return nil, dsUnitError{"Unsupprted mime type: " + mimeType + " on " + url}
+}
+
+//CreateDatasets crate a datasets from passed in data resources
+func (f datasetFactoryImpl) CreateDatasets(dataResource *DatasetResource) (*Datasets, error) {
+
+	storage, err := storage.NewServiceForURL(dataResource.URL, dataResource.Credential)
+	if err != nil {
+		return nil, err
+	}
+	candidates, err := storage.List(dataResource.URL)
+	if err != nil {
+		return nil, err
+	}
+
+	var datasets = make([]*Dataset, 0)
+	for _, cadidate := range candidates {
+
+		_, name := path.Split(cadidate.URL())
+		if dataResource.Prefix != "" {
+			if !strings.HasPrefix(name, dataResource.Prefix) {
+				continue
+			}
+			name = string(name[len(dataResource.Prefix):])
+		}
+		if dataResource.Postfix != "" {
+			if !strings.HasSuffix(name, dataResource.Postfix) {
+				continue
+			}
+			name = string(name[:len(name)-len(dataResource.Postfix)])
+		}
+		if strings.Index(name, ".") != -1 {
+			name = string(name[:strings.Index(name, ".")])
+		}
+		reader, err := storage.Download(cadidate)
+		if err != nil {
+			return nil, err
+		}
+		ext := path.Ext(cadidate.URL())
+		if ext != "" {
+			ext = string(ext[1:])
+		}
+		data, err := f.createDataset(reader, dataResource.Datastore, name, "text/"+ext, cadidate.URL())
+		if err != nil {
+			return nil, err
+		}
+		datasets = append(datasets, data)
+	}
+	return &Datasets{
+		Datastore: dataResource.Datastore,
+		Datasets:  datasets,
+	}, nil
+
 }
 
 func newDatasetFactory(managerRegistry dsc.ManagerRegistry) DatasetFactory {
