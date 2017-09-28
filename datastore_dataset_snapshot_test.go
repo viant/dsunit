50348c46578daeaa2a1e30bcf8506dae3bb6f082
adrianwit <adrianwit@gmail.com>
2017-09-27 23:06:26 -0700

    updated test

--- datastore_dataset_snapshot_test.go
+++ datastore_dataset_snapshot_test.go
@@ -7,18 +7,22 @@ import (
 	"testing"
 
 	"github.com/stretchr/testify/assert"
+	"os"
 )
 
 func TestDatastoreDatasetSnapshotTaker_Take(t *testing.T) {
 
-	dbRegistry := dsc.NewManagerRegistry()
-	managerFactory := dsc.NewManagerFactory()
-	config := dsc.NewConfig("mysql", "[user]:[password]@[url]", "user:root,password:***,url:tcp(127.0.0.1:3306)/test?parseTime=true")
-	dbManager, _ := managerFactory.Create(config)
-	dbRegistry.Register("test", dbManager)
 
-	snapshotManager := dsunit.NewDatastoreDatasetSnapshotManager(dbRegistry)
-	assert.NotNil(t, snapshotManager)
-	//err :=snapshotManager.Take("test", dsunit.ExpandTestProtocolAsPathIfNeeded("test://test/snapshot"))
-	//assert.Nil(t, err)
+	if os.Getenv("TestDatastoreDatasetSnapshotTaker") != "" {
+		dbRegistry := dsc.NewManagerRegistry()
+		managerFactory := dsc.NewManagerFactory()
+		config := dsc.NewConfig("mysql", "[user]:[password]@[url]", "user:root,password:****,url:tcp(127.0.0.1:3306)/mydb1?parseTime=true")
+		dbManager, _ := managerFactory.Create(config)
+		dbRegistry.Register("mydb1", dbManager)
+
+		snapshotManager := dsunit.NewDatastoreDatasetSnapshotManager(dbRegistry)
+		assert.NotNil(t, snapshotManager)
+		err := snapshotManager.Take("mydb1", "/tmp/mydb1")
+		assert.Nil(t, err)
+	}
 }
