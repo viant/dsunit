639f7f4a071ade7106ec30106136d30b1f39cc26
tkrishnaviant <tkrishna@viantinc.com>
2019-02-05 16:51:13 -0800

    Update helper.go

--- helper.go
+++ helper.go
@@ -1,37 +1,99 @@
 package dsunit
 
 import (
+	"bytes"
 	"fmt"
 	"github.com/viant/dsc"
 	"github.com/viant/toolbox"
+	"github.com/viant/toolbox/data"
+	"github.com/viant/toolbox/storage"
+	"github.com/viant/toolbox/url"
 	"path"
 	"strings"
 )
 
-func recreateTables(registry dsc.ManagerRegistry, datastore string) error {
+func getDatastoreTables(registry dsc.ManagerRegistry, datastore string) ([]string, error) {
 	manager := registry.Get(datastore)
 	dialect := GetDatastoreDialect(datastore, registry)
-	var tables = []string{}
-	var err error
 	if hasDatastore(manager, dialect, datastore) {
-		tables, err = dialect.GetTables(manager, datastore)
-		if err != nil {
+		return dialect.GetTables(manager, datastore)
+	}
+	return []string{}, nil
+}
+
+func getRegistryTables(registry dsc.ManagerRegistry, datastore string) []string {
+	manager := registry.Get(datastore)
+	tableRegistry := manager.TableDescriptorRegistry()
+	if len(tableRegistry.Tables()) > 0 {
+		return tableRegistry.Tables()
+	}
+	return []string{}
+}
+
+func indexTables(tables []string) map[string]bool {
+	var index = make(map[string]bool)
+	for _, table := range tables {
+		index[table] = true
+	}
+	return index
+}
+
+func dropTables(registry dsc.ManagerRegistry, datastore string, tables []string) error {
+	manager := registry.Get(datastore)
+	dialect := GetDatastoreDialect(datastore, registry)
+	for _, table := range tables {
+		if err := dialect.DropTable(manager, datastore, table); err != nil {
 			return err
 		}
 	}
-	var existingTables = make(map[string]bool)
-	toolbox.SliceToMap(tables, existingTables, toolbox.CopyStringValueProvider, toolbox.TrueValueProvider)
+	return nil
+}
+
+//recreateTables recreate registry or all datastorre table, or just create new if drop flag is false
+func recreateTables(registry dsc.ManagerRegistry, datastore string, drop bool) error {
+	manager := registry.Get(datastore)
+	dialect := GetDatastoreDialect(datastore, registry)
+	registryTables := getRegistryTables(registry, datastore)
+	dbTables, err := getDatastoreTables(registry, datastore)
+	if err != nil {
+		return err
+	}
+	if drop {
+		if len(registryTables) == 0 { //drop all database tables
+			if err = dropTables(registry, datastore, dbTables); err != nil {
+				return err
+			}
+		} //drop only registry specified tables
+		registryTablesExisting := make([]string, 0)
+		toolbox.Intersect(registryTables, dbTables, &registryTablesExisting)
+		if len(registryTablesExisting) > 0 {
+			fmt.Printf("Dropping from %s following tables %v \n", datastore, registryTablesExisting)
+		} else {
+			fmt.Println("No existing tables to Drop in datastore " + datastore)
+		}
+		if err = dropTables(registry, datastore, registryTablesExisting); err != nil {
+			return err
+		}
+	}
+
+	//no registry table - quit
+
+	if len(registryTables) == 0 {
+		return nil
+	}
+	dbTables, err = getDatastoreTables(registry, datastore)
+	if err != nil {
+		return err
+	}
+	existingTable := indexTables(dbTables)
 	tableRegistry := manager.TableDescriptorRegistry()
-	for _, table := range tableRegistry.Tables() {
+	for _, table := range registryTables {
 		descriptor := tableRegistry.Get(table)
 		if !descriptor.HasSchema() {
 			continue
 		}
-		if _, found := existingTables[table]; found {
-			err := dialect.DropTable(manager, datastore, table)
-			if err != nil {
-				return err
-			}
+		if _, hasTable := existingTable[table]; hasTable {
+			continue
 		}
 		err := dialect.CreateTable(manager, datastore, table, "")
 		if err != nil {
@@ -151,4 +213,32 @@ func discoverBaseURLAndPrefix(operation string) (string, string) {
 	}
 	method = convertToLowerUnderscore(method)
 	return parent, fmt.Sprintf(name+"_%v_%v_", method, operation)
+}
+
+func escapeVariableIfNeeded(val string) string {
+	if strings.HasPrefix(val, "$$") { //escaping
+		val = strings.Replace(val, "$$", "$", 1)
+	}
+	return val
+}
+
+func expandDataIfNeeded(context toolbox.Context, records []map[string]interface{}) {
+	if context.Contains(SubstitutionMapKey) {
+		var substitutionMap *data.Map
+		if context.GetInto(SubstitutionMapKey, &substitutionMap) {
+			for i, record := range records {
+				records[i] = toolbox.AsMap(substitutionMap.Expand(record))
+			}
+		}
+	}
+}
+
+func uploadContent(resource *url.Resource, response *BaseResponse, payload []byte) {
+	storageService, err := storage.NewServiceForURL(resource.URL, resource.Credentials)
+	if err != nil {
+		response.SetError(err)
+		return
+	}
+	err = storageService.Upload(resource.URL, bytes.NewReader(payload))
+	response.SetError(err)
 }
