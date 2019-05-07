a5576f9a9cd49ace6a749f646979481e05c9f62d
adrianwit <adrianwit@gmail.com>
2019-05-07 14:01:12 -0700

    updated example

--- example/ora/ora_test.go
+++ example/ora/ora_test.go
@@ -2,43 +2,33 @@ package example
 
 import (
 	"fmt"
+	_ "github.com/mattn/go-oci8"
 	"github.com/stretchr/testify/assert"
 	"github.com/viant/dsc"
 	"github.com/viant/dsunit"
 	"github.com/viant/endly"
 	"github.com/viant/endly/system/docker"
 	"github.com/viant/toolbox"
-	_ "gopkg.in/rana/ora.v4"
-	"os"
-	"path"
 	"strings"
 	"testing"
-	"time"
 )
 
 /*
 Prerequisites:
-1.docker service running
+1. docker service running
+2. Oracle instance client SDK.(client and sdk)
 
-	export CGO_CFLAGS=-I/opt/oracle/instantclient_12_1/sdk/include/
-	export CGO_LDFLAGS=/opt/oracle/instantclient_12_1/lib/
+	https://github.com/mattn/go-oci8
 
+	go get github.com/mattn/go-oci8
 
-3. Oracle instance client SDK.(client and sdk)
-	Instant Client Package - Basic: All files required to run OCI, OCCI, and JDBC-OCI applications
-	Instant Client Package - SDK: Additional header files and an example makefile for developing Oracle applications with Instant Client
-4.  go get gopkg.in/rana/ora.v4
 */
 
 //Global variables for all test integrating with endly.
 var endlyManager = endly.New()
 var endlyContext = endlyManager.NewContext(toolbox.NewContext())
-var localhostCredential = path.Join(os.Getenv("HOME"), ".secret/localhost.json")
 
-func init() {
-	os.Setenv("ORACLE_HOME", "/opt/oracle/instantclient_12_1")
-	os.Setenv("TNS_ADMIN", "/opt/oracle/instantclient_12_1/admin")
-}
+
 
 func setup(t *testing.T) {
 
@@ -60,6 +50,9 @@ func tearDown(t *testing.T) {
 func TestDsunit_Oracle(t *testing.T) {
 	setup(t)
 	defer tearDown(t)
+
+//	showCreateTable()
+
 	if dsunit.InitFromURL(t, "config/init.json") {
 		if !dsunit.PrepareFor(t, "mydb", "data", "use_case_1") {
 			return
@@ -72,8 +65,30 @@ func TestDsunit_Oracle(t *testing.T) {
 	}
 }
 
+func showCreateTable() error {
+	config, err := dsc.NewConfigWithParameters("oci8", "[username]/[password]@127.0.0.1:1521/xe", "ora-e2e", nil)
+	if err != nil {
+		return err
+	}
+	manager, err := dsc.NewManagerFactory().Create(config)
+	if err != nil {
+		return err
+	}
+	dialect := dsc.GetDatastoreDialect("oci8")
+	DDL, err := dialect.ShowCreateTable(manager,  "events")
+
+	dd := strings.Replace(DDL, "events", "events_993999", 1)
+	dd = strings.Replace(dd, ";", "", 1)
+
+	_, err =manager.Execute(dd )
+	fmt.Printf("DML: %v %v\n", DDL, err)
+	return nil
+}
+
+
+
 func runSomeBusinessLogic() error {
-	config, err := dsc.NewConfigWithParameters("ora", "mydb/oracle@127.0.0.1:1521/xe", "", nil)
+	config, err := dsc.NewConfigWithParameters("oci8", "mydb/oracle@127.0.0.1:1521/xe", "", nil)
 	if err != nil {
 		return err
 	}
@@ -98,7 +113,7 @@ func runSomeBusinessLogic() error {
 func startOracle() error {
 
 	_, err := endlyManager.Run(endlyContext, &docker.RunRequest{
-		Image:  "wnameless/oracle-xe-11g:latest",
+		Image:  "thebookpeople/oracle-xe-11g",
 		Env: map[string]string{
 			"ORACLE_ALLOW_REMOTE": "true",
 		},
@@ -107,40 +122,6 @@ func startOracle() error {
 			"1521": "1521",
 		},
 	})
-	if err != nil {
-		return err
-	}
-
-	//it takes some time to docker container to fully start
-
-	//user/passw@host:port/sid
-	config, err := dsc.NewConfigWithParameters("ora", "system/oracle@127.0.0.1:1521/xe", "", nil)
-	if err != nil {
-		return err
-	}
-
-	dscManager, err := dsc.NewManagerFactory().Create(config)
-	if err != nil {
-		return err
-	}
-	defer dscManager.ConnectionProvider().Close()
-	for i := 0; i < 60; i++ {
-		var record = make(map[string]interface{})
-		_, err = dscManager.ReadSingle(&record, "SELECT 1 AS name FROM dual", nil, nil)
-		if err == nil {
-			time.Sleep(2 * time.Second)
-			break
-		}
-		//wait for docker service to fully start
-		if !(strings.Contains(err.Error(), "TNS:connection closed") ||
-			strings.Contains(err.Error(), "ORA-12537") ||
-			strings.Contains(err.Error(), "ORA-12514") ||
-			strings.Contains(err.Error(), "ORA-01033") ||
-			strings.Contains(err.Error(), "bad connection")) {
-			return err
-		}
-		time.Sleep(4 * time.Second)
-	}
 	return err
 }
 
